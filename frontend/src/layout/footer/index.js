0686ebb39b50f792067a3a22e56daca5b4b60677
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-06 23:26:39 +0200

    chore: create header and footer components

---
+++ frontend/src/layout/footer/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+
+import './style.scss';
+
+export default () => {
+
+    return(
+        <footer id="main-footer">
+            <article className='container'>
+                <h3>Spinning violently around the y-axis in the footer</h3> 
+            </article>
+        </footer>
+    )
+}
---
+++ frontend/src/layout/header/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+
+import './style.scss';
+
+export default () => {
+
+
+    return(
+        <header>
+            <article className='container'>
+                <h1>Generating witty dialog in the header</h1>
+            </article>
+        </header>
+    );
+}
