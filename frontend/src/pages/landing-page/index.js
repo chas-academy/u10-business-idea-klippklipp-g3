1d4ccae3810c9355165897d25c39bd1e3acd4419
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-06 23:27:05 +0200

    chore: create landing page component

---
+++ frontend/src/pages/landing-page/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.scss';
+
+const LandingPage = () => {
+
+    return(
+        <>
+            <h1>Tokenizing real life in landing page</h1>
+            <p>Non sequitur.</p>
+        </>
+    );
+}
+
+export default LandingPage;
