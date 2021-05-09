0ab476a095a7d35a3d3783ccacbe76a396d970c2
Axel Roussille Åberg <986301+axelra82@users.noreply.github.com>
2021-05-10 01:04:40 +0200

    chore: update header in layout

--- frontend/src/layout/header/index.js
+++ frontend/src/layout/header/index.js
@@ -1,16 +1,86 @@
-/* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import StoreContext from '../../context/StoreContext';
+import { LoginForm, SignupForm } from '../../components/auth';
 
 import './style.scss';
 
-export default () => {
+const LayoutHeader = () => {
+	const {
+		store: {
+			modals: { toggleModal, updateModal },
+			user: { setAuth, isAuthed, unAuth },
+		},
+	} = useContext(StoreContext);
 
+	const setLoginModal = () => {
+		toggleModal();
 
-    return(
-        <header>
-            <article className='container'>
-                <h1>Generating witty dialog in the header</h1>
-            </article>
-        </header>
-    );
-}
+		updateModal({
+			title: 'Login',
+			body: <LoginForm />,
+		});
+	};
+
+	const setSignupModal = () => {
+		toggleModal();
+
+		updateModal({
+			title: 'Signup',
+			body: <SignupForm />,
+		});
+	};
+
+	const logoutUser = () => {
+		unAuth();
+		setAuth(false);
+	};
+
+	return (
+		<header>
+			<nav className='container-fluid navbar navbar-light bg-light p-1'>
+				<Link to='/' className='navbar-brand'>
+					U10G3
+				</Link>
+
+				<ul className='navbar-nav me-auto'>
+					{isAuthed && (
+						<li className='nav-item'>
+							<Link to='/user' className='nav-link'>
+								Profile
+							</Link>
+						</li>
+					)}
+				</ul>
+				{!isAuthed ? (
+					<>
+						<button
+							type='button'
+							className='btn btn-outline-primary'
+							onClick={setLoginModal}
+						>
+							login
+						</button>
+						<button
+							type='button'
+							className='btn btn-outline-info ms-2'
+							onClick={setSignupModal}
+						>
+							Signup
+						</button>
+					</>
+				) : (
+					<button
+						type='button'
+						className='btn btn-primary'
+						onClick={logoutUser}
+					>
+						logout
+					</button>
+				)}
+			</nav>
+		</header>
+	);
+};
+
+export default LayoutHeader;
