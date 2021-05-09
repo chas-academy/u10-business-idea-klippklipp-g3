e9fa815d01184a43b5db928b0cff2b2c821f6edb
Axel Roussille Åberg <986301+axelra82@users.noreply.github.com>
2021-05-10 01:04:24 +0200

    chore: update footer in layout

--- frontend/src/layout/footer/index.js
+++ frontend/src/layout/footer/index.js
@@ -1,15 +1,44 @@
-/* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
+import StoreContext from '../../context/StoreContext';
 
 import './style.scss';
 
-export default () => {
+const LayoutFooter = () => {
+	const {
+		store: {
+			layouts: { setLayout },
+		},
+	} = useContext(StoreContext);
 
-    return(
-        <footer id="main-footer">
-            <article className='container'>
-                <h3>Spinning violently around the y-axis in the footer</h3> 
-            </article>
-        </footer>
-    )
-}
+	const footerRef = useRef(0);
+
+	useEffect(() => {
+		setLayout((layout) => {
+			return {
+				...layout,
+				footer: {
+					height: footerRef.current.clientHeight,
+				},
+			};
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [footerRef]);
+
+	return (
+		<footer ref={footerRef} id='main-footer' className='bg-dark'>
+			<article className='container text-light'>
+				<div className='row'>
+					<div className='col'>
+						Spinning violently around the y-axis in the footer
+					</div>
+					<div className='col'>
+						Hum something loud while others stare
+					</div>
+					<div className='col'>Follow the white rabbit</div>
+				</div>
+			</article>
+		</footer>
+	);
+};
+
+export default LayoutFooter;
