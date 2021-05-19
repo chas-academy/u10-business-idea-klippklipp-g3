6cb69a9798f2309dd3728f144e669a0ac8928cf3
Fijo-Lurk <filip.johansson@chasacademy.se>
2021-05-20 00:10:09 +0200

    installing font awesome icons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Main } from './layout';
 import ContextProvider from './provider/ContextProvider';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faAirFreshener } from '@fortawesome/free-solid-svg-icons';
 
 import './styling/style.scss';
+
+library.add(fas, faAirFreshener);
 
 ReactDOM.render(
 	<BrowserRouter>
